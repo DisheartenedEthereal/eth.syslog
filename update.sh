2bf7f06f8b92e13d978cece0983017435010b9bf
Ethereal <dethereal@protonmail.com>
2023-01-12 19:38:56 +0330

    Commit before pushing to surge

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build && surge --domain eth-syslog.surge.sh _site/
+git add . && git commit -m "Commit before pushing to surge" && git push && bundle exec jekyll build && surge --domain eth-syslog.surge.sh _site/
